004b37103a666f7b4f1d1ac680647020ce2377bc
Scot Sexton <rss3509@gmail.com>
2020-05-19 04:19:02 +0000

    Done.

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,10 +5,11 @@ def join_nested_strings(mixed_data)
     element_index = 0 
     while element_index < mixed_data[row_index].count do 
       if mixed_data[row_index][element_index].class == String
-      
+        nested_strings << mixed_data[row_index][element_index] + " "
       end 
+      element_index += 1 
    end
-    nested_strings << mixed_data[row_index][element_index] + ""
+   
     row_index += 1 
   end
   nested_strings
